ce9ef515a450a0d573e6d64cef75db241056b509
govindkurmi <govind877045@gmail.com>
2024-05-03 20:22:16 +0530

    added logic added user class

---
+++ src/main/java/com/gk/model/Role.java
@@ -0,0 +1,23 @@
+package com.gk.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+}
---
+++ src/main/java/com/gk/model/User.java
@@ -0,0 +1,41 @@
+package com.gk.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    @NotEmpty
+    @Email(message = "{errors.invalid_email}")
+    private String email;
+    @Column(nullable = false)
+    @NotEmpty
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
+
+}
